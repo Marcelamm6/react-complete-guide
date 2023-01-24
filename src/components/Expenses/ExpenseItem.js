e2be254235c1ac9ec501c25761102bbd755a62be
marcelamm6@gmail.com <marcelamm6@gmail.com>
2023-01-23 22:49:28 -0300

    renderingLists malditos bugs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,10 +13,10 @@ const ExpenseItem = (props) => {
   }
 
   return (
-    <Card className="expense-item">
+    <Card className="expense-item" >
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
